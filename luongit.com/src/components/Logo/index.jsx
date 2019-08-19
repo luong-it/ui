c9f1d4404730ad24eba6342ac12e62098c569578
Dat Nguyen <tdat.nguyen93@gmail.com>
2019-08-18 20:51:29 -0700

    Init Project Skeleton

---
+++ luongit.com/src/components/Logo/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <div className="logo-main">Lương IT</div>
+      <div className="logo-slogan">Cùng nhau chia sẽ, cải thiện nhu nhập</div>
+    </div>
+  )
+}
